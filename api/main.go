11b9f8f9823b81478004a91d212821a96563e6cd
Casey Clyde <caseysclyde@gmail.com>
2022-07-13 00:30:40 -0700

    update endpoints test

--- api/main.go
+++ api/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("api/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("api/greet/", func(w http.ResponseWriter, r *http.Request) {
 		name := r.URL.Path[len("/greet/"):]
 		fmt.Fprintf(w, "Hello %s\n", name)
 	})
