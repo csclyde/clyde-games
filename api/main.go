22ac1fe6502dbda922cfd6f993949c4dbe5c2d7c
Casey Clyde <caseysclyde@gmail.com>
2022-07-13 00:31:09 -0700

    update again

--- api/main.go
+++ api/main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	http.HandleFunc("api/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		fmt.Fprintf(w, "Hello World - test")
 	})
 
 	http.HandleFunc("api/greet/", func(w http.ResponseWriter, r *http.Request) {
 		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
+		fmt.Fprintf(w, "Hello Mr. %s\n", name)
 	})
 
 	http.ListenAndServe(":9990", nil)
