c181e8ccbcd629efed42ddbdabe11c0f14e76d05
Casey Clyde <caseysclyde@gmail.com>
2022-07-13 20:52:30 -0700

    basic feedback page

---
+++ src/Feedback.svelte
@@ -0,0 +1,32 @@
+<script>
+
+	async function getFeedback() {
+		const res = await fetch(`https://clyde.games/api/feedback`);
+		const feedback = await res.json();
+
+		if (res.ok) {
+			return feedback;
+		} else {
+			throw new Error(feedback);
+		}
+	}
+
+	let promise = getFeedback();
+</script>
+
+<main>
+	<h1>Player Feedback</h1>
+	<hr/>
+
+	{#await promise}
+		<p>loading...</p>
+	{:then feedback}
+		<p>{JSON.stringify(feedback)}</p>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+</main>
+
+<style>
+
+</style>
