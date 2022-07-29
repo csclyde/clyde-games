3d2ad9b7bd46c5e2fe0d5b94a6db05a33e0490af
Casey Clyde <caseysclyde@gmail.com>
2022-07-29 16:42:55 -0700

    add stats to analysis page

--- site/src/pages/AnalyzeWords.svelte
+++ site/src/pages/AnalyzeWords.svelte
@@ -38,13 +38,31 @@
 
 	let analyzedText = '';
 
-	let stats = getWordStats();
+	let statsPromise = getWordStats();
+
+	function pct(n) {
+		return (Math.floor(n * 1000) / 10)
+	}
 </script>
 
 <main>
 	<h2>Analyze Word Origins</h2>
 	<hr/>
-	<small>{JSON.stringify(stats)}</small>
+	{#await statsPromise}
+		<p>loading...</p>
+	{:then stats}
+	<section>
+		<small>Total: {stats.Total}</small>
+		<small>Unknown: {pct(stats.Unknown / stats.Total)}%</small>
+		<small>Germanic: {pct(stats.Germanic / stats.Total)}%</small>
+		<small>French: {pct(stats.French / stats.Total)}%</small>
+		<small>Latin: {pct(stats.Latin / stats.Total)}%</small>
+		<small>Greek: {pct(stats.Greek / stats.Total)}%</small>
+		<small>Other: {pct(stats.Other / stats.Total)}%</small>
+	</section>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
 
 	<textarea bind:value={analyzedText}></textarea>
 	<button on:click={analyzeWords}>Analyze</button>
@@ -61,5 +79,10 @@
 	textarea {
 		width: 50%;
 		height: 300px;
+	}
+
+	section {
+		display: flex;
+		gap: 16px;
 	}
 </style>
